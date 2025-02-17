140be201375e742e52dd3fe2bed2edc3c3c60822
caulog <caulog20@wfu.edu>
2025-02-16 22:15:51 -0500

    hw main

--- HW4/log_sales_ogc2111/log_sales.js
+++ HW4/log_sales_ogc2111/log_sales.js
@@ -1,2 +1,21 @@
-//java
-
+//javascript
+$( function() {
+    $( ".draggable" ).draggable({
+      revert: "invalid" // revert to og position
+    });
+    $( ".trash" ).droppable({
+      over: function(event, ui) {
+          $(this).addClass("can-drop");
+        },
+        out: function(event, ui) {
+          $(this).removeClass("can-drop");
+        },
+      drop: function( event, ui ) {
+        $( this )
+          .removeClass("can-drop")
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+          .html( "Dropped!" );
+      }
+    });
+  } );
