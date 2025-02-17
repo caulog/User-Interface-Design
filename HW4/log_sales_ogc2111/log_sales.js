03683a65ac7e9fdd9014900a0bbab634e4f52704
caulog <caulog20@wfu.edu>
2025-02-16 22:33:47 -0500

    client list

--- HW4/log_sales_ogc2111/log_sales.js
+++ HW4/log_sales_ogc2111/log_sales.js
@@ -1,5 +1,33 @@
 //javascript
 $( function() {
+  // add clients from clients.js
+  let clients = [
+    "Shake Shack",
+    "Toast",
+    "Computer Science Department",
+    "Teacher's College",
+    "Starbucks",
+    "Subsconsious",
+    "Flat Top",
+    "Joe's Coffee",
+    "Max Caffe",
+    "Nussbaum & Wu",
+    "Taco Bell",
+  ];
+
+    // Apply jQuery UI autocomplete
+  $("#clientName").autocomplete({
+    source: clients
+  });
+
+  // Open the autocomplete list when the input is focused
+  $("#clientName").focus(function() {
+    $(this).autocomplete("search", "");  // This triggers the autocomplete dropdown
+    $(this).autocomplete("widget").show();  // Force the autocomplete dropdown to show
+  });
+
+
+
     $( ".draggable" ).draggable({
       revert: "invalid" // revert to og position
     });
